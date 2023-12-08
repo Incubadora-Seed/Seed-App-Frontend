76ae4bd325ee5bd9961f48ecfacf9d5c3d2918ee
Maxine <robekmax123@gmail.com>
2023-12-08 14:35:29 -0300

    Update index.js

--- src/app/(telas)/index.js
+++ src/app/(telas)/index.js
@@ -5,8 +5,6 @@ export default function Home() {
     return (
         <View>
             <Text>Home</Text>
-            <Link href='/users/1'>Go to user 1</Link>
-            <Pressable onPress={() => router.push('/users/2')}><Text>Go to user 2</Text></Pressable>
         </View>
     )
 }
