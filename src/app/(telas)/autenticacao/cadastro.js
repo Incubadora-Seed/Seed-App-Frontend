ac39997141b218837ee72ff8ef32b588adb44dfc
Maxine <robekmax123@gmail.com>
2023-12-12 17:16:07 -0300

    Update cadastro.js

--- src/app/(telas)/autenticacao/cadastro.js
+++ src/app/(telas)/autenticacao/cadastro.js
@@ -10,16 +10,16 @@ import Dropdown1 from '../../../components/Dropdown1.js';
 // Tela: https://www.figma.com/file/f2Vsc2XnqkxnL9iYGbFvXz/APP-SEED?type=design&node-id=21-355&mode=design&t=OcnMSN4vulBDbebg-4
 
 export default function Login() {
-    const [dropdownContaSelected, setDropdownContaSelected] = useState('')
+    const [tipoConta, setTipoConta] = useState('')
     const dropdownContaData = [{ key: '1', value: 'Empresa' }, { key: '2', value: 'Aluno' }]
-    const [dropdownCursoSelected, setDropdownCursoSelected] = useState('')
+    const [curso, setCurso] = useState('')
     const dropdownCursoData = [{ key: '1', value: 'Informática' }, { key: '2', value: 'Eletromecânica' }]
 
     const formAluno = (
         <>
             <Input placeholder='Nome Completo' icon='person-outline' style={{ marginBottom: 35 }} />
             <Input placeholder='E-mail' icon='mail-outline' inputMode='email' style={{ marginBottom: 35 }} />
-            <Dropdown1 placeholder='Curso' icon='book-outline' data={dropdownCursoData} setSelected={setDropdownCursoSelected} style={{ marginBottom: 35 }} />
+            <Dropdown1 placeholder='Curso' icon='book-outline' data={dropdownCursoData} setSelected={setCurso} style={{ marginBottom: 35 }} />
             <Input placeholder='Ano que está cursando' icon='calendar-outline' inputMode='numeric' style={{ marginBottom: 35 }} />
             <Input placeholder='Senha' icon='lock-closed-outline' style={{ marginBottom: 35 }} />
             <Input placeholder='Confirme a Senha' icon='shield-checkmark-outline' />
@@ -35,10 +35,10 @@ export default function Login() {
                     <Text style={styles.txtTitle}>Criar Conta</Text>
                 </View>
                 <View style={styles.viewInputs}>
-                    <Dropdown1 placeholder='Tipo de Conta' icon='people-outline' data={dropdownContaData} setSelected={setDropdownContaSelected} style={{ marginBottom: 35 }} />
-                    {dropdownContaSelected == 'Aluno' && formAluno}
+                    <Dropdown1 placeholder='Tipo de Conta' icon='people-outline' data={dropdownContaData} setSelected={setTipoConta} style={{ marginBottom: 35 }} />
+                    {tipoConta == 'Aluno' && formAluno}
                 </View>
-                {dropdownContaSelected && <Button1 text='Cadastrar' />}
+                {tipoConta && <Button1 text='Cadastrar' />}
             </View>
         </ScrollView>
     )
