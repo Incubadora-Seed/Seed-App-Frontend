f8522659f35a9f895b1a653ca68b3cf526e927f6
ArthurEv <arthurev.sg034@academico.ifsul.edu.br>
2024-01-10 13:10:35 -0300

    tela de perfil

---
+++ src/styles/Perfil.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+    viewTopo: {
+        "backgroundColor": "#4D4DFF",
+        "paddingVertical": 15,
+        alignItems: "center"
+    },
+    nomeUsusario: {
+        "fontSize": 32,
+        "fontFamily": "Poppins_500Medium"
+    },
+    viewEditar: {
+        "fontSize": 24,
+        "fontFamily": "Poppins_500Medium",
+        flexDirection: "row",
+    }
+})
